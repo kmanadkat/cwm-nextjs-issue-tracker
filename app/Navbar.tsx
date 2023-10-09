8ed2af27d93e7e5f343bda395ff33d349d1337d2
kmanadkat <kmanadkat@gmail.com>
2023-10-09 12:28:43 +0530

    feat: Active Links Styled In Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
+import classNames from 'classnames';
 
 const Navbar = () => {
+	const currentPath = usePathname();
+	console.log(currentPath);
 	const links = [
 		{ label: 'Dashboard', href: '/' },
 		{ label: 'Issues', href: '/issues' },
@@ -19,7 +25,11 @@ const Navbar = () => {
 				{links.map((l) => (
 					<Link
 						key={l.href}
-						className='text-zinc-500 hover:text-zinc-800 transition-colors'
+						className={classNames({
+							'text-zinc-900': l.href === currentPath,
+							'text-zinc-500': l.href !== currentPath,
+							'hover:text-zinc-800 transition-colors': true,
+						})}
 						href={l.href}>
 						{l.label}
 					</Link>
