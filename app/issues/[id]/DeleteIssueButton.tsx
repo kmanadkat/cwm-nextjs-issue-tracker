b1f31bbbb68cac94dea87e262f71918a0d4cf2e0
kmanadkat <kmanadkat@gmail.com>
2023-10-23 19:31:53 +0530

    feat: Added Delete Issue Confirmation Dialog Box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,36 @@
+'use client';
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 	return (
-		<Button color='red'>
-			<TrashIcon />
-			Delete Issue
-		</Button>
+		<AlertDialog.Root>
+			<AlertDialog.Trigger>
+				<Button color='red'>
+					<TrashIcon />
+					Delete Issue
+				</Button>
+			</AlertDialog.Trigger>
+			<AlertDialog.Content>
+				<AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+				<AlertDialog.Description>
+					Are your sure you want to delete this issue? This action cannot be
+					undone.
+				</AlertDialog.Description>
+				<Flex gap='3' mt='4' justify='end'>
+					<AlertDialog.Cancel>
+						<Button variant='soft' color='gray'>
+							Cancel
+						</Button>
+					</AlertDialog.Cancel>
+					<AlertDialog.Action>
+						<Button variant='solid' color='red'>
+							Delete Issue
+						</Button>
+					</AlertDialog.Action>
+				</Flex>
+			</AlertDialog.Content>
+		</AlertDialog.Root>
 	);
 };
 
